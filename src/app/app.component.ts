2bc6df85938187581976e88ae7ed0f93bcd532c5
huongnvtv <huongitnv@gmail.com>
2021-09-30 21:33:11 +0700

    commit two way biding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,11 +6,5 @@ import { Component, Input, VERSION } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  agreed = 0;
-  disagreed = 0;
-  voters = ['Narco', 'Celeritas', 'Bombasto'];
-  onVoted(agree: boolean) {
-    if (agree == true) this.agreed++;
-    else this.disagreed++;
-  }
+  name = 'demo';
 }
